85ae70623c99690d10b290888ea8536aed02dd7c
ivvist <ivvist@gmail.com>
2022-06-17 16:56:54 +0300

    misc

--- .github/scripts/git-release.sh
+++ .github/scripts/git-release.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-branch_name=$(git symbolic-ref -q HEAD)
-branch_name=${branch_name##refs/heads/}
-branch_name=${branch_name:-HEAD}
-echo "branch_name=$branch_name"
-
 branchname="main"
 # Get version from tag
 s='-'
